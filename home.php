85caf9366a2fa78e3dd462c544f559ed5d17efe7
Gina Chang <gchang37@gmail.com>
2019-11-09 13:53:18 -0800

    modified files

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>deCodeCongestion</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        {% include "header.php" %}
+        <h1>Decode Congestion</h1>
+        <p>We suggest</p>
+
+        {% include "foot.php"%}
+
+    </body>
+</html>
