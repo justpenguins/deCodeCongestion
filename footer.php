0d59b9281aab1f79acc17723b0a3b7ec087c5ec0
Gina Chang <gchang37@gmail.com>
2019-11-09 22:32:04 -0800

    removed report page

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <div class="footer">
     <h1 class="goteamgo">GO TEAM GO</h1>
 </div>
-<p> &copy; Our product name </p>
+<p> &copy; ReRout 2019 </p>
--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <div class="topnav">
     <a class="active" href="home.php">Home</a>
-    <a href="report.php">Report a Problem</a>
     <a href="admin.php">Admin</a>
 </div>
