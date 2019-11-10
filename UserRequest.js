6d541eea819e863bbcf9a0d18b2cff1893c16666
Paul Chiu <paul@dhcp-206-12-35-198.ubcvisitor.wireless.ubc.ca>
2019-11-09 16:18:24 -0800

    fixed input value for jquery

--- UserRequest.js
+++ UserRequest.js
@@ -1,9 +1,10 @@
 let uid = "unit1";
-let endPoint = $('#endPoint').val();
-let startPoint = $('#startPoint').val();
-let busNumber = $('#start').val();
 
 $('.submit_button').click(function(event) {
+    let startPoint = $('#startPoint').val();
+    let endPoint = $('#endPoint').val();
+    let busNumber = $('#start').val();
+
     console.log({
         "uid": uid,
         "End Point": endPoint,
@@ -13,8 +14,6 @@ $('.submit_button').click(function(event) {
 });
 
 $('.report_button').click(function(event) {
-    console.log("Hello World");
-
     let routeNumber = $('#routeNumber').val();
     let busNumberOne = $('#busNumberOne').val();
     let busNumberTwo = $('#busNumberTwo').val();
@@ -44,8 +43,8 @@ $('.report_button').click(function(event) {
 });
 
 $(document).on('click', '.alternativeRoute' , function() {
-    let endPoint = $(this).find('.endPoint').text();
     let startPoint = $(this).find('.startPoint').text();
+    let endPoint = $(this).find('.endPoint').text();
     let busNumber = $(this).find('.busNumber').text();
 
     //make a call to update the user on the bus
